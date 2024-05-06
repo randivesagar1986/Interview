2e0ce90a970f6dd8e85599768247064ed2376bf7
randivesagar1986 <127309320+randivesagar1986@users.noreply.github.com>
2024-05-06 13:52:06 +0530

    Add files via upload

---
+++ TallenticaSoftwareInterview.js
@@ -0,0 +1,35 @@
+
+//Introduction
+//why came this a, c, b
+//function exwcution context
+console.log("a");
+setTimeout(()=>{
+    console.log("b");
+})
+Promise.resolve("c").then((val)=>{
+    console.log(val)
+})
+
+//Why react is better than other framework.
+
+//client and sever side rendering
+
+//HOW data flow in react
+
+//what is state management
+
+//div and span diff
+
+//box modal
+
+//Difference between Array and object
+
+//Why react is better
+
+//How we can make our application for all the Asset
+
+//server and client side rendering
+
+//How React is better than vanillAJS
+
+//Synthetic Event.
